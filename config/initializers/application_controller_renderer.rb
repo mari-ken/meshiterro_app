981a1316dd49e7ba7270ff510826d6718692815a
mari-ken <m.kenju1030@gmail.com>
2021-03-11 05:11:27 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '00fbe5bf1eb38495278cd2301f17716f65a5fda88f43d0467d89be3fa1924534a48a6062fe0f5ac1e38029e08b70830e5cb458cf753990bb9e0149b0d5da09c7'
